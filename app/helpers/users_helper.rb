b8e1425e615b1eff8a465cc24ae7935168686a8e
Matt Bee <matt@todayishould.com>
2018-03-20 22:40:27 +0000

    Add User Profle page and gravatar image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def gravatar_for(user, size: 50)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: gravatar_alt_text(user.name), class: "gravatar")
+  end
+
+  private
+
+  def gravatar_alt_text(name)
+    "#{name} gravatar profile picture"
+  end
 end
