f0c0c9fca30b6ab126f02414b989951c3caa216d
Matt Bee <matt@todayishould.com>
2018-04-23 22:12:03 +0100

    Add remember_digest to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,15 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
-  
+
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
   end
 end
